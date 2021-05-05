35c4d0babee0fe412ddc1f577c6e68d7bcfbc746
Aneesh Panda <aneeshpanda.ap@gmail.com>
2021-05-05 09:52:06 +0530

    Changes to Typeform

--- index.js
+++ index.js
@@ -18,12 +18,15 @@
 document.querySelector(".next").addEventListener("click", function (event) {
   if (document.myform.weathertype.value === "Yes") {
     document.querySelector("#or").innerHTML =
-      "<div class='questions container-fluid'> <div class='row pt-5'> <div class=' col-lg-2'> </div> <div class=' col-lg-1 pl-5'> <h1>Q4.</h1> </div> <div class=' col-lg-6'> <h1>What is the weather like at the place you want to go?</h1> <select class='mt-5 pt-2 pb-2' name='weather'> <option value=' '> </option> <option value='Sunny'>Sunny</option> <option value='Chilly'>Chilly</option> <option value='Rainy'>Rainy</option> <option value='Snowy'>Snowy</option> <option value='Dry'>Dry</option> </select> <br> <input name='submit' class='btn getstarted mt-lg-5' type='submit' value='Submit'></form> </div> <div class=' col-lg-3'> </div> </div> </div>";
+      "<div class='questions container-fluid'> <div class='row pt-5'> <div class=' col-lg-2'> </div> <div class=' col-lg-1 pl-5'> <h1>Q4.</h1> </div> <div class=' col-lg-6'> <h1>What is the weather like at the place you want to go?</h1> <form name='myform1' action='/' method='post'> <select class='mt-5 pt-2 pb-2' name='weather'> <option value=' '> </option> <option value='Sunny'>Sunny</option> <option value='Chilly'>Chilly</option> <option value='Rainy'>Rainy</option> <option value='Snowy'>Snowy</option> <option value='Dry'>Dry</option> </select> <br> <input name='submit' class='btn getstarted mt-lg-5' type='submit' value='Submit'> </form> </div> <div class=' col-lg-3'> </div> </div> </div>";
   } else if (document.myform.weathertype.value === "No") {
     document.querySelector("#or").innerHTML +=
-      "<div class='questions container-fluid'> <div class='row pt-5'> <div class=' col-lg-2'> </div> <div class=' col-lg-1 pl-5'> <h1>Q4.</h1> </div> <div class=' col-lg-6'> <h1>From: </h1> <input class='location mt-5 pt-2 pb-2' name='from' type='text'><br> <a class='next btn getstarted mt-lg-5' href='#to' role='button'>Next</a></div> <div class=' col-lg-3'> </div> </div> </div> <div id = 'to' class='questions container-fluid'> <div class='row pt-5'> <div class=' col-lg-2'> </div> <div class=' col-lg-1 pl-5'> <h1>Q5.</h1> </div> <div class=' col-lg-6'> <h1>To:</h1> <input class='location mt-5 pt-2 pb-2' name='to' type='text'> <br> <input name='submit' class='btn getstarted mt-lg-5' type='submit' value='Submit'></form> </div> <div class=' col-lg-3'> </div> </div> </div>";
+      "<div class='questions container-fluid'> <div class='row pt-5'> <div class=' col-lg-2'> </div> <div class=' col-lg-1 pl-5'> <h1>Q4.</h1> </div> <div class=' col-lg-6'> <h1>From: </h1> <form name='myform1' action='/' method='post'> <input class='location mt-5 pt-2 pb-2' name='from' type='text'><br> <a class='next btn getstarted mt-lg-5' href='#to' role='button'>Next</a>  </form> </div> <div class=' col-lg-3'> </div> </div> </div> <div id = 'to' class='questions container-fluid'> <div class='row pt-5'> <div class=' col-lg-2'> </div> <div class=' col-lg-1 pl-5'> <h1>Q5.</h1> </div> <div class=' col-lg-6'> <h1>To:</h1> <form name='myform2' action='/' method='post'> <input class='location mt-5 pt-2 pb-2' name='to' type='text'> <br> <input name='submit' class='btn getstarted mt-lg-5' type='submit' value='Submit'> </form> </div> <div class=' col-lg-3'> </div> </div> </div>";
   }
 });
+
+
+
 
 
 
